c88551f1b7c8829777913e01f1fd89c1ac975740
Natasha <natasha@publicsource.org>
2016-11-21 13:58:34 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,201 @@
+//Baldwin
+$('#baldwin').on("click", function(){
+	
+
+	$('#schoolTitle').html("Baldwin-Whitehall");
+
+	$('#waterTested').html("Never");
+	$("#waterElevated").html("N/A");
+	$("#waterActions").html("N/A");
+
+	$('#paintTested').html("Never");
+	$("#paintElevated").html("N/A");
+	$("#paintActions").html("N/A");
+
+	$('#radonTested').html("Never");
+	$("#radonElevated").html("N/A");
+	$("#radonActions").html("N/A");
+});
+
+//Bethel
+$('#bethel').on("click", function(){
+	$('#schoolTitle').html("Bethel Park");
+
+	$('#waterTested').html("Never");
+	$("#waterElevated").html("N/A");
+	$("waterActions").html("N/A");
+
+	$('#paintTested').html("Never");
+	$("#paintElevated").html("N/A");
+	$("#paintActions").html("N/A");
+
+	$('#radonTested').html("Never");
+	$("#radonElevated").html("N/A");
+	$("#radonActions").html("N/A");
+});
+
+//lebanon
+$('#lebanon').on("click", function(){
+	$('#schoolTitle').html("Mt. Lebanon");
+
+	$('#waterTested').html("2016");
+	$("#waterElevated").html('<i class="fa fa-times" aria-hidden="true"></i>');
+	$("#waterActions").html("N/A");
+
+	$('#paintTested').html("2010");
+	$("#paintElevated").html('<i class="fa fa-check" aria-hidden="true"></i>');
+	$("#paintActions").html('<i class="fa fa-check" aria-hidden="true"></i>');
+
+	$('#radonTested').html("2015");
+	$("#radonElevated").html('<i class="fa fa-check" aria-hidden="true"></i>');
+	$("#radonActions").html('<i class="fa fa-check" aria-hidden="true"></i>');
+});
+
+//allegheny
+$('#allegheny').on("click", function(){
+	$('#schoolTitle').html("North Allegheny");
+
+	$('#waterTested').html("2016");
+	$("#waterElevated").html("Above zero; below EPA action-level of 15ppb (15ppb is EPA's action-level for municipalities. Their official guideline for schools remains 20ppb. WHO's guidelines suggest keeping it below 10ppb; the American Academy of Pediatrics put out a statement in June urging all schools to have lead water concentrations below 1ppb.)");
+	$("#waterActions").html('<i class="fa fa-times" aria-hidden="true"></i>');
+
+	$('#paintTested').html("2015");
+	$("#paintElevated").html('<i class="fa fa-check" aria-hidden="true"></i>');
+	$("#paintActions").html('<i class="fa fa-times" aria-hidden="true"></i>');
+
+	$('#radonTested').html("2003");
+	$("#radonElevated").html('<i class="fa fa-check" aria-hidden="true"></i>');
+	$("#radonActions").html('<i class="fa fa-times" aria-hidden="true"></i>');
+});
+
+//hills
+$('#hills').on("click", function(){
+	$('#schoolTitle').html("North Hills");
+
+	$('#waterTested').html("Never");
+	$("#waterElevated").html("N/A");
+	$("#waterActions").html("N/A");
+
+	$('#paintTested').html("Never");
+	$("#paintElevated").html("N/A");
+	$("#paintActions").html("N/A");
+
+	$('#radonTested').html("2016");
+	$("#radonElevated").html('<i class="fa fa-times" aria-hidden="true"></i>');
+	$("#radonActions").html("N/A");
+});
+
+//pine
+$('#pine').on("click", function(){
+	$('#schoolTitle').html("Pine-Richland");
+
+	$('#waterTested').html("2016");
+	$("#waterElevated").html('<i class="fa fa-times" aria-hidden="true"></i>');
+	$("#waterActions").html("N/A");
+
+	$('#paintTested').html("Never");
+	$("#paintElevated").html("N/A");
+	$("#paintActions").html("N/A");
+
+	$('#radonTested').html("2016");
+	$("#radonElevated").html('<i class="fa fa-times" aria-hidden="true"></i>');
+	$("#radonActions").html("N/A");
+});
+
+//pittsburgh
+$('#pittsburgh').on("click", function(){
+	$('#schoolTitle').html("Pittsburgh");
+
+	$('#waterTested').html("2016");
+	$("#waterElevated").html('<i class="fa fa-check" aria-hidden="true"></i>');
+	$("#waterActions").html('<i class="fa fa-check" aria-hidden="true"></i>');
+
+	$('#paintTested').html("2015");
+	$("#paintElevated").html('<i class="fa fa-check" aria-hidden="true"></i>');
+	$("#paintActions").html('<i class="fa fa-check" aria-hidden="true"></i>');
+
+	$('#radonTested').html("2006-2007");
+	$("#radonElevated").html('<i class="fa fa-check" aria-hidden="true"></i>');
+	$("#radonActions").html('<i class="fa fa-check" aria-hidden="true"></i>');
+});
+
+//plum
+$('#plum').on("click", function(){
+	$('#schoolTitle').html("Plum Borough");	
+
+	$('#waterTested').html("Never");
+	$("#waterElevated").html("N/A");
+	$("#waterActions").html("N/A");
+
+	$('#paintTested').html("Never");
+	$("#paintElevated").html("N/A");
+	$("#paintActions").html("N/A");
+
+	$('#radonTested').html("Never");
+	$("#radonElevated").html("N/A");
+	$("#radonActions").html("N/A");
+});
+
+//shaler
+$('#shaler').on("click", function(){
+	$('#schoolTitle').html("Shaler Area");
+
+	$('#waterTested').html("2016");
+	$("#waterElevated").html('<i class="fa fa-times" aria-hidden="true"></i>');
+	$("#waterActions").html("N/A");
+
+	$('#paintTested').html("Never");
+	$("#paintElevated").html("N/A");
+	$("#paintActions").html("N/A");
+
+	$('#radonTested').html("2015");
+	$("#radonElevated").html('<i class="fa fa-check" aria-hidden="true"></i>');
+	$("#radonActions").html('<i class="fa fa-check" aria-hidden="true"></i>');
+});
+
+//clair
+$('#clair').on("click", function(){
+	$('#schoolTitle').html("Upper St. Clair");
+
+	$('#waterTested').html("2016");
+	$("#waterElevated").html('<i class="fa fa-times" aria-hidden="true"></i>');
+	$("#waterActions").html("N/A");
+
+	$('#paintTested').html("2008");
+	$("#paintElevated").html('<i class="fa fa-check" aria-hidden="true"></i>');
+	$("#paintActions").html('<i class="fa fa-check" aria-hidden="true"></i>');
+
+	$('#radonTested').html("2011");
+	$("#radonElevated").html('<i class="fa fa-times" aria-hidden="true"></i>');
+	$("#radonActions").html("N/A");
+});
+
+//fox
+$('#fox').on("click", function(){
+
+
+	$('#schoolTitle').html("Fox Chapel");
+
+	$('#waterTested').html("Never");
+	$("#waterElevated").html("N/A");
+	$("#waterActions").html("N/A");
+
+	$('#paintTested').html("2012");
+	$("#paintElevated").html('<i class="fa fa-times" aria-hidden="true"></i>');
+	$("#paintActions").html("N/A");
+
+	$('#radonTested').html("2013");
+	$("#radonElevated").html('<i class="fa fa-times" aria-hidden="true"></i>');
+	$("#radonActions").html("N/A");
+});
+
+$(".school").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#schoolTitle").offset().top},
+        'slow');
+});
+ 
+
+ $('.school').click(function(){
+ 	$('.infoBox').slideIn();
+ });
